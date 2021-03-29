3c13e55100a081816dab5c02190d050e2fef34ad
Denner Augusto De Paula Bernardes <denner.augusto90@gmail.com>
2021-03-29 16:12:24 -0300

    melhoria no envio de formulario

--- javascript/javascript.js
+++ javascript/javascript.js
@@ -1,1 +1,19 @@
-$("#mobile_open").click(()=>{})
+$('#name').val('')
+$('#email').val('')
+$('#mensagem_form').val('')
+
+$('form').submit(function () {
+    $('#button').text('enviado com sucesso')
+});
+
+
+$("#button").click(() => {
+    if ($('#name').val() == '' || $('#email').val() == '' || $(('#mensagem_form').val() == '')) {
+        $('#name').focus()
+        $('#email').focus()
+        $('#mensagem_form').focus()
+    }
+})
+
+
+
