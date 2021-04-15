0e64dfcee3ae07e8b6dd288d537cdc8620cd0c24
Denner Augusto De Paula Bernardes <denner.augusto90@gmail.com>
2021-04-15 11:35:26 -0300

    messagem de inicio da pagina e envio de form

--- javascript/javascript.js
+++ javascript/javascript.js
@@ -2,20 +2,28 @@ $('#name').val('')
 $('#email').val('')
 $('#mensagem_form').val('')
 
-$('form').submit(function () {
-    $('#button').text('enviado com sucesso')
-    location.reload()
-
-});
-
-
 $("#button").click(() => {
     if ($('#name').val() == '' || $('#email').val() == '' || $(('#mensagem_form').val() == '')) {
         $('#name').focus()
         $('#email').focus()
-        $('#mensagem_form').focus()
+        $('#mensagem_form').focus() 
     }
 })
 
+$('form').submit(function () {
+    $('#button').text('enviado com sucesso')
+    location.reload()
+    Swal.fire('tudo certo muito obrigado')
+});    
 
+
+window.onload(
+    swal.fire({
+        button
+    },
+        'sejá bem vindo',
+        'este é um projeto ainda em desenvolvimento e pode conter falhas, caso tenha alguma sugestão em como podemos melhorar mande um email para nós no final da pagina',
+        'error'
+    )
+)
 
